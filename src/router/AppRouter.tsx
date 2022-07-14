6c849b3122f05a62c1ed54048ba1f753ca191b2b
Boris Zarate <boris.zlobos@gmail.com>
2022-07-14 17:01:03 -0400

    added: routers

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth/pages"
+import { CalendarPage } from "../calendar/pages"
+
+export const AppRouter = () => {
+
+  const validPage: string = 'authenticated'
+
+  return (
+    <Routes>
+      {
+        validPage === 'unauthenticated' 
+          ? <Route path='/auth/*' element={ <LoginPage /> } />
+          : <Route path='/*' element={ <CalendarPage /> } />
+      }
+    </Routes>
+  )
+}
