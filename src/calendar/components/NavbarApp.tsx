59037f8c989e359284d6c2e4cf08d39de0921b01
Boris Zarate <boris.zlobos@gmail.com>
2022-10-03 16:23:48 -0300

    fix: navbar

--- src/calendar/components/NavbarApp.tsx
+++ src/calendar/components/NavbarApp.tsx
@@ -1,24 +1,22 @@
-import { Grid, Text, Button } from '@mantine/core'
+import { Button, Box } from '@mantine/core'
 import { UserCircle } from 'tabler-icons-react'
 
 export const NavbarApp = () => {
-
   return (
     <>
-      <Grid sx={{ alignItems: 'center', backgroundColor: '#E7F5FF' }}>
-        <Grid.Col span={8}>
-          <Text ml={16} weight={700}>
-            User
-          </Text>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Text align='right'>
-            <Button variant='subtle' mr={16} leftIcon={<UserCircle />}>
-              Logout
-            </Button>
-          </Text>
-        </Grid.Col>
-      </Grid>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          backgroundColor: '#228BE6',
+        }}
+      >
+        <Box>
+          <Button leftIcon={<UserCircle />}>
+            Logout
+          </Button>
+        </Box>
+      </Box>
     </>
   )
 }
