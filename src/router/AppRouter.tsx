cf5d76d8e7bf0650b37c3e37c03e840de04dd623
Boris Zarate <boris.zlobos@gmail.com>
2022-10-20 20:57:19 -0300

    fix: redirect calendar page when the use is authenticated

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,18 +1,30 @@
 import { Route, Routes } from 'react-router-dom'
-import { CalendarPage } from '../calendar/pages/CalendarPage'
-import { LoginPage } from '../auth/pages/LoginPage'
-import { checkAuthToken } from '../api/userAuth'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { CalendarPage } from '../calendar/pages'
+import { LoginPage } from '../auth/pages'
+import {checkAuthToken} from '../api/userAuth'
 
 export const AppRouter = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const getTokenValidation = localStorage.getItem('token')
+    if (!getTokenValidation) {
+      return navigate('/auth')
+    }
+  }, [])
+
   useEffect(() => {
     checkAuthToken()
   }, [])
 
+  console.log(checkAuthToken)
+
   return (
     <Routes>
-      <Route path='/' element={<CalendarPage />} />
-      <Route path='/auth' element={<LoginPage />} />
+        <Route path='/' element={<CalendarPage />} />
+        <Route path='/auth' element={<LoginPage />} />
     </Routes>
   )
 }
